0bc93267c9fdc35eff4392800200a436fef4ee9d
Brian Stephenson <stephenson.brian15@gmail.com>
2022-05-28 19:46:56 -0400

    Regex made readable

--- Tools/Push_Filter_Changes.ps1
+++ Tools/Push_Filter_Changes.ps1
@@ -1,5 +1,5 @@
-$lab = "C:\Users\steph\OneDrive\Default\Documents\Projects\POE\"
+$lab = "C:\Users\steph\OneDrive\Default\Documents\Projects\POE\Filters"
 $Filter = "$lab\TestFilter.filter"
 $live = "C:\Users\steph\OneDrive\Default\Documents\My Games\Path of Exile\"
 
-Copy-Item $Filter -Destination $live\TestFilter.filter -Verbose -Force -ErrorAction SilentlyContinue
+Copy-Item $Filter -Destination $live\TestFilter.filter -Verbose -Force
