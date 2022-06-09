e1a9b34a2f5ab9f9faa72d4b4b80b5baa7c96e88
Brian Stephenson <stephenson.brian15@gmail.com>
2022-06-09 12:56:02 -0400

    endgame filter started

--- Tools/Push_Filter_Changes.ps1
+++ Tools/Push_Filter_Changes.ps1
@@ -1,5 +1,5 @@
 $lab = "C:\Users\steph\OneDrive\Default\Documents\Projects\POE\Filters"
-$Filter = "$lab\TestFilter.filter"
+$Filter = "$lab\endgame.filter"
 $live = "C:\Users\steph\OneDrive\Default\Documents\My Games\Path of Exile\"
 
-Copy-Item $Filter -Destination $live\TestFilter.filter -Verbose -Force
+Copy-Item $Filter -Destination $live\endgame.filter -Verbose -Force
